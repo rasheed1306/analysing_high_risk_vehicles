ab72061c662100a95345faba9899ba35aca4c80d
Rasheed Mohammed <rasheedmohammed2006@gmail.com>
2025-04-06 16:20:53 +1000

    Count unique crashes
    Counted unique crashes for each unique year of manufacture, body style and manufacturer.
    Can allow us to see relationship between vehicles and crashes.

--- task3_1.py
+++ task3_1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('a1-datasets/filtered_vehicle_test.csv', 'r') as file: 
+with open('a1-datasets/filtered_vehicle.csv', 'r') as file: 
     reader = csv.reader(file)
     # Skips Header row
     header = next(reader) 
@@ -14,8 +14,15 @@ with open('a1-datasets/filtered_vehicle_test.csv', 'r') as file:
 pd.set_option('display.max_columns', None)
 
 # Convert the list of rows into a DataFrame
-df = pd.DataFrame(rows, columns=header)
-print(df[['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
+df = pd.DataFrame(rows[:11], columns=header)
 
-# Counting number of unique crashes for each year of vehicle manufacture
-# df['VEHICLE_YEAR_MANUF'] = df['VEHICLE_YEAR_MANUF'].value_counts()
+print(df.loc[df.index, ['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
+
+# Counting number of unique crashes for each year of vehicle manufacture, body style, and manufacturer
+counts_manuf = df['VEHICLE_YEAR_MANUF'].value_counts()
+counts_style = df['VEHICLE_BODY_STYLE'].value_counts()
+counts_make = df['VEHICLE_MAKE'].value_counts()
+
+print(counts_manuf)
+print(counts_style)
+print(counts_make)

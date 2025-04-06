4c46bb9642bcc45ec66d1b7ba0e14a0bcad8dd09
Rasheed Mohammed <rasheedmohammed2006@gmail.com>
2025-04-06 18:04:17 +1000

    Represent High Risk Vehicle Categories graphically
    Graphs relationship between the year of manufacture of vehicles in crashes and there unique body style and maker.
    Can be used to identify trends in crashes relating to unique vehicles.
    
    This allows one to see

--- task3_1.py
+++ task3_1.py
@@ -18,7 +18,7 @@ df = pd.DataFrame(rows[:11], columns=header)
 
 print(df.loc[df.index, ['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
 
-# Counting number of unique crashes for each year of vehicle manufacture, body style, and manufacturer
+# Count number crashes for each car's unique year of manufacture, body style, and manufacturing company
 counts_manuf = df['VEHICLE_YEAR_MANUF'].value_counts()
 counts_style = df['VEHICLE_BODY_STYLE'].value_counts()
 counts_make = df['VEHICLE_MAKE'].value_counts()
@@ -26,3 +26,21 @@ counts_make = df['VEHICLE_MAKE'].value_counts()
 print(counts_manuf)
 print(counts_style)
 print(counts_make)
+
+# Graphically represent correlation between year of manufacture and unique vehicle types in crashes
+# Can identify how vehicles safety has improved over the years
+
+# Group unique vehicle types (body style and maker)
+types = zip(df['VEHICLE_BODY_STYLE'], df['VEHICLE_MAKE'])
+
+# Convert vehicle types to string and concatenate
+y = [type[0] + ' ' + type[1] for type in types]
+
+# Convert year of manufacture to int
+x = pd.to_numeric(df['VEHICLE_YEAR_MANUF'], errors='coerce').astype('Int64')
+
+plt.scatter(x, y)
+plt.xlabel('Year of Vehicle Manufacture ')
+plt.ylabel('Vehicle Types (Body Style and Maker)') 
+plt.title('Correlation between Year of Manufacture and Vehicle Types in Crashes')
+plt.show()

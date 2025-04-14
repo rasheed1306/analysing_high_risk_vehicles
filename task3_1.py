9016933cadedf5457ce2408e52a13eec1e814aac
Rasheed Mohammed <rasheedmohammed2006@gmail.com>
2025-04-14 17:19:16 +1000

    Calulate optimal clustering group

--- task3_1.py
+++ task3_1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-DEBUG = False
+DEBUG = True    
 
 with open('a1-datasets/filtered_vehicle.csv', 'r') as file: 
     reader = csv.reader(file)
@@ -18,37 +18,41 @@ pd.set_option('display.max_columns', None)
 # Convert the list of rows into a DataFrame
 df = pd.DataFrame(rows[:11], columns=header)
 
-if DEBUG:
-    print(df.loc[df.index, ['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
 
-# Count number crashes for each car's unique year of manufacture, body style, and manufacturing company
-counts_manuf = df['VEHICLE_YEAR_MANUF'].value_counts()
-counts_style = df['VEHICLE_BODY_STYLE'].value_counts()
-counts_make = df['VEHICLE_MAKE'].value_counts()
+def task3_1(): 
+    if DEBUG:
+        print(df[['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
+    # Count number crashes for each car's unique year of manufacture, body style, and manufacturing company
+    counts_manuf = df['VEHICLE_YEAR_MANUF'].value_counts()
+    counts_style = df['VEHICLE_BODY_STYLE'].value_counts()
+    counts_make = df['VEHICLE_MAKE'].value_counts()
 
-print(counts_manuf)
-print(counts_style)
-print(counts_make)
+    print(counts_manuf)
+    print(counts_style)
+    print(counts_make)
 
-# Graphically represent correlation between year of manufacture and unique vehicle types in crashes
-# Can identify how vehicles safety has improved over the years
+    # Graphically represent correlation between year of manufacture and unique vehicle types in crashes
+    # Can identify how vehicles safety has improved over the years
 
-# Group unique vehicle types (body style and maker)
-types = zip(df['VEHICLE_BODY_STYLE'], df['VEHICLE_MAKE'])
+    # Group unique vehicle types (body style and maker)
+    types = zip(df['VEHICLE_BODY_STYLE'], df['VEHICLE_MAKE'])
 
-# Convert vehicle types to string and concatenate
-y = [type[0] + ' ' + type[1] for type in types]
+    # Convert vehicle types to string and concatenate
+    y = [type[0] + ' ' + type[1] for type in types]
 
-# Convert year of manufacture to int
-x = pd.to_numeric(df['VEHICLE_YEAR_MANUF'], errors='coerce').astype('Int64')
+    # Convert year of manufacture to int
+    x = pd.to_numeric(df['VEHICLE_YEAR_MANUF'], errors='coerce').astype('Int64')
 
-plt.scatter(x, y)
-plt.xlabel('Year of Vehicle Manufacture ')
-plt.ylabel('Vehicle Types (Body Style and Maker)') 
-plt.title('Correlation between Year of Manufacture and Vehicle Types in Crashes')
+    plt.scatter(x, y)
+    plt.xlabel('Year of Vehicle Manufacture ')
+    plt.ylabel('Vehicle Types (Body Style and Maker)') 
+    plt.title('Correlation between Year of Manufacture and Vehicle Types in Crashes')
 
-if DEBUG:
-    plt.show()
+    if DEBUG:
+        plt.show()
 
-# Save the figure as a PNG file 
-plt.savefig('task3_1_scatter.png')
+    # Save the figure as a PNG file 
+    plt.savefig('task3_1_scatter.png')
+
+if __name__ == "__main__":
+    task3_1()
--- task3_2.py
+++ task3_2.py
@@ -1,8 +1,68 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
+from task3_1 import df
 import pandas as pd
 import numpy as np
-from task3_1 import df
+import matplotlib.pyplot as plt
 
-print(df.loc[df.index, ['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
+DEBUG = True
 
+# Truncating values to 2 decimal places 
+pd.set_option('display.float_format', '{:.2f}'.format)
+
+# Grouping vehicles by unique Year of Vehicle Manufacture, Body Style and Manufacturer combination
+if DEBUG:
+    print(df[['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE']])
+
+# Changing Vehicle Year of Manufacture and Features to int
+df['VEHICLE_YEAR_MANUF'] = pd.to_numeric(df['VEHICLE_YEAR_MANUF'], errors='coerce')
+df['NO_OF_CYLINDERS'] = pd.to_numeric(df['NO_OF_CYLINDERS'], errors='coerce')
+df['NO_OF_WHEELS'] = pd.to_numeric(df['NO_OF_WHEELS'], errors='coerce')
+df['SEATING_CAPACITY'] = pd.to_numeric(df['SEATING_CAPACITY'], errors='coerce')
+df['TARE_WEIGHT'] = pd.to_numeric(df['TARE_WEIGHT'], errors='coerce')
+df['TOTAL_NO_OCCUPANTS'] = pd.to_numeric(df['TOTAL_NO_OCCUPANTS'], errors='coerce')
+
+unique_vehicles = df.groupby(['VEHICLE_YEAR_MANUF', 'VEHICLE_BODY_STYLE', 'VEHICLE_MAKE'])
+
+
+# Printing aggregrate statistics for each group 
+
+features = ['NO_OF_CYLINDERS', 'NO_OF_WHEELS', 'SEATING_CAPACITY', 'TARE_WEIGHT', 'TOTAL_NO_OCCUPANTS']
+
+unique_vehicles_agg = unique_vehicles[features].mean()
+
+if DEBUG:
+    print("Grouped Year of Manufacture, Body Style and Manufacturer Aggregated Data")
+    print(unique_vehicles_agg)
+
+# Max min normalising data 
+scaler = MinMaxScaler()
+unique_vehicles_norm = scaler.fit_transform(unique_vehicles_agg)
+
+if DEBUG:
+    # Printing normalised data
+    print("Grouped Year of Manufacture Normalised Data")
+    print(np.round(unique_vehicles_norm, 2))
+
+
+# Performing KMeans clustering on the normalised data
+distortions = []
+k_range = range(1, 10)
+for k in k_range:
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(unique_vehicles_norm)
+    distortions.append(kmeans.inertia_)
+
+plt.plot(k_range, distortions, marker='o')
+plt.title('Elbow Method for Optimal k')
+plt.xlabel('Number of clusters (k)')
+plt.ylabel('Distortion')
+plt.grid()
+
+if DEBUG:
+    plt.show()
+
+# The elbow method suggests that the optimal number of clusters is at k = 5
+
+# Save the figure as a PNG file
+plt.savefig('task3_2_elbow.png')
